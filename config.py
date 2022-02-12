e91802f1db79b71ef1232cb777d9a610813d959f
King Amda <86409667+King-Amda@users.noreply.github.com>
2022-02-12 20:45:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+YOUR_USERNAME = os.getenv("YOUR_USERNAME")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL")
+UPDATE_GROUP = os.getenv("UPDATE_GROUP")
